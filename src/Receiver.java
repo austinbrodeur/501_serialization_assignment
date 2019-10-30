8a72f402ea497d58f6d9b6f39f58250b9c5e840f
Austin Brodeur <austinbrodeur@shaw.ca>
2019-10-30 17:07:25 -0600

    Created classes, created socket method

---
+++ src/Receiver.java
@@ -0,0 +1,12 @@
+import java.net.*;
+import java.util.*;
+import java.nio.file.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.net.DatagramSocket;
+
+public class Receiver extends NetworkUtils {
+
+}
